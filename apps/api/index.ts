6ed964dd1792b8e3d9d371bf2799f0df2019d8d3
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-04-02 02:31:05 +0530

    api setup

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,22 +1,53 @@
 import express from "express";
+import { authMiddleware } from "./middleware";
+import { prismaClient } from "db/client";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/website" , (req,res) => {
+app.post("/api/v1/website", authMiddleware, async (req, res) => {
+  const userId = req.userId!; //if you are sure that something surely exists then use !
+  const url = req.body; //whenever we use req.body we have to use app.use(express.json())
+  const data = await prismaClient.website.create({
+    ///we put the await in a constant
+    data: {
+      userId,
+      url,
+    },
+  });
 
-})
+  res.json({
+    id: data.id, //importanttttt
+  });
+});
 
-app.get("/api/v1/website/status" , (req,res) => {
-    
-})
+app.get("/api/v1/website/status", authMiddleware, async (req, res) => {
+  const websiteId = req.query.websiteId as unknown as string //query paramters can either be string or array so we defined
+  const userId = req.userId
+  const data = await prismaClient.website.findFirst({
+    where: {
+        id: websiteId,
+        userId: userId
+    },
+    include:{
+        ticks: true   //the ticks array we mentioned
+    }
+  })
+  res.json({
+    data 
+  })
+});
 
-app.get("/api/v1/website/status" , (req,res) => {
-    
-})
+app.get("/api/v1/websites", authMiddleware, async (req, res) => {
+    const userId = req.userId;
 
-app.delete("/api/v1/website/status" , (req,res) => {
-    
-})
+   const data =  await prismaClient.website.findMany({
+        where: {
+            userId
+        }
+    })
+});
 
+app.delete("/api/v1/website", authMiddleware, (req, res) => {});
 
 app.listen(3000);
